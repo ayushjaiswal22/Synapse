d9ebb50839f862b6ee4a30d7041df15d2b7546fc
aZij1Cam <jan.zacharias@dfki.de>
2017-08-18 19:23:01 +0200

    Added license to our contributions

--- lib/roi_data_layer/minibatch.py
+++ lib/roi_data_layer/minibatch.py
@@ -1,9 +1,20 @@
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
+"""
+    py-faster-rcnn-ft - Custom Training of Deep Learning Models for Image Classification
+    Copyright (C) 2017  DFKI GmbH
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 """Compute minibatch blobs for training a Fast R-CNN network."""
 
--- tools/prototxt_mod.py
+++ tools/prototxt_mod.py
@@ -1,4 +1,20 @@
+"""
+    py-faster-rcnn-ft - Custom Training of Deep Learning Models for Image Classification
+    Copyright (C) 2017  DFKI GmbH
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 from google.protobuf import text_format
 import caffe.proto.caffe_pb2 as caffe_pb2
 import os
