756b675b7bda38cf2aa03adad15ae416ae2da261
aZij1Cam <30466964+aZij1Cam@users.noreply.github.com>
2017-08-28 14:45:58 +0200

    updated demo.py

--- tools/demo.py
+++ tools/demo.py
@@ -49,7 +49,7 @@ import customToolbox
 # TODO: Automatically assign latest caffemodel here
 #  List pretrainend model you want to run the demo on here
 NETS = {'vgg16': ('VGG16',
-                  'vgg_cnn_m_1024_faster_rcnn_iter_10.caffemodel'),
+                  'vgg_cnn_m_1024_faster_rcnn_iter_490000.caffemodel'),
         'zf': ('ZF',
                   'ZF_faster_rcnn_final.caffemodel')
         }
